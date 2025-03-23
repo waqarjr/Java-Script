3c6a4f2a026166002c14b6d889cff421d223f089
waqarjr <waqarjr03@gmail.com>
2025-03-23 08:02:19 +0500

    commit

---
+++ introduction1.js
@@ -0,0 +1,12 @@
+// what is a Java Script Language 
+// JavaScript is a dynamic, high-level programming language used to create interactive web applications.
+//  It runs in browsers and servers (via Node.js) to handle logic, events, and API requests. 
+// JavaScript is essential for front-end and back-end development in modern web applications.
+
+
+// History of Java Script 
+// JavaScript was created by Brendan Eich in 1995 at Netscape in just 10 days to make web pages interactive.
+// Initially called Mocha, then LiveScript, it was later renamed JavaScript to align with Java's popularity.
+// Over time, JavaScript evolved with ECMAScript standards, becoming the backbone of modern web development.
+
+
